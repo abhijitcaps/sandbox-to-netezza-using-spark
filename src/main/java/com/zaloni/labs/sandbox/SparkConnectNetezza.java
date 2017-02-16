c95abf2fd38682e640f91fc95b81b6bfe15a7693
adas <adas@zaloni.com>
2017-02-16 17:01:35 +0530

    export to netezza from hive using spark

---
+++ src/main/java/com/zaloni/labs/sandbox/SparkConnectNetezza.java
@@ -0,0 +1,42 @@
+package com.datatype.conversion;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.hive.HiveContext;
+
+import java.util.List;
+import java.util.Properties;
+
+public class SparkConnectNetezza{
+    private static final SparkContext sc =
+            new SparkContext(new SparkConf().setAppName("SparkSaveToDb").setMaster("local[*]"));
+
+    private static final SQLContext sqlContext = new HiveContext(sc);
+
+    public static void main(String[] args) throws Exception{
+
+        final String jdbcUrl = args[0];
+        System.out.println("destinationLocation :: "+jdbcUrl);
+
+        /*
+        Source table, from where metadata and data will be fetched. This is from Hive in our scenario
+         */
+        //String hiveSchemaNameDotTableName = "schemaName.TableName"; //Hive SchemaName.TableName
+        String hiveSchemaNameDotTableName = "pranab_schema.Pranab_Delimited_1";
+
+        sqlContext.setConf("hive.metastore.filter.hook", "org.apache.hadoop.hive.metastore.DefaultMetaStoreFilterHookImpl");
+
+        String hql = "select * from "+hiveSchemaNameDotTableName;
+        DataFrame usersDf = sqlContext.sql(hql);
+        System.out.print("Data Frame :: "+usersDf); // Output is => Data Frame :: [id: int, name: string]
+        final SQLContext sqlc = new HiveContext(sc);
+        Properties prop = new Properties();
+        prop.setProperty("url", jdbcUrl);
+        prop.setProperty("driver", "org.netezza.Driver");
+        sqlc.setConf(prop);
+        DataFrame usersDf1 = sqlc.read().json(usersDf.toJSON());
+        usersDf1.write().mode("overwrite").jdbc(jdbcUrl, "destinationTableName", prop);
+    }
+}
